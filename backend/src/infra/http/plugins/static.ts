4afc002e10df0adcf26b681c32af1092d6146a83
Pedro Henrique <reis08897@gmail.com>
2025-05-31 16:04:46 -0300

    fix: fastifyStatic

--- backend/src/infra/http/plugins/static.ts
+++ backend/src/infra/http/plugins/static.ts
@@ -1,10 +1,22 @@
 import { FastifyInstance } from "fastify";
 import { join } from "path";
 import fastifyStatic from "@fastify/static"
+import { existsSync } from "fs";
 
 export async function staticFilesPlugin(fastify: FastifyInstance) {
-    fastify.register(fastifyStatic, {
-        root: join(__dirname, '..', '..', '..', 'uploads'),
-        prefix: '/uplods/'
-    })
+    try {
+       const pathToUploads = join(__dirname, '..', '..', '..', '..', 'uploads');
+        console.log("Static folder path:", pathToUploads);
+        console.log('Path:', pathToUploads);
+        console.log('Exists:', existsSync(pathToUploads));
+
+        fastify.register(fastifyStatic, {
+            root: pathToUploads,
+            prefix: '/uploads/',
+        });
+
+        console.log("Static files plugin registered!");
+    } catch (error) {
+        console.error("Failed to register static files plugin:", error);
+    }
 }
