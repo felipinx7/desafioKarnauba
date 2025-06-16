412376d3fda6c08560db500c87c95502aac2fe56
Felipe Lima <flimalimafelipek@gmail.com>
2025-06-16 16:03:26 -0300

    fix: apibaseurl corrected

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // Configure axios instance, with baseURL, that will be used for all requests and withCredentials set to true
 export const api = axios.create({
-    baseURL: "http://localhost:4444",
+    baseURL: "https://karnaubaapi.onrender.com",
     withCredentials: true,
 });
