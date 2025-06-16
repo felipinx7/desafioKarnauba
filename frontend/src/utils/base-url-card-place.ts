cfe2ed57868fb0f4be54bc7b248d4fb708781db8
Felipe Lima <flimalimafelipek@gmail.com>
2025-06-16 16:29:00 -0300

    fix: updated photo

--- frontend/src/utils/base-url-card-place.ts
+++ frontend/src/utils/base-url-card-place.ts
@@ -3,7 +3,7 @@ import { CardPlacesDTO } from '@/dto/places/data-card-placesDTO'
 
 export function BaseUrlPlaces(photoURL: string, props: CardPlacesDTO) {
   const photoUrl =
-    props.photos.length > 0 ? `http://localhost:4444/uploads/${photoURL}` : backgroundloginpage
+    props.photos.length > 0 ? `https://karnaubaapi.onrender.com/uploads/${photoURL}` : backgroundloginpage
 
   return photoUrl
 }
--- frontend/src/utils/base-url-photo.ts
+++ frontend/src/utils/base-url-photo.ts
@@ -4,7 +4,7 @@ import { CardEventPageAdministrative } from "@/dto/event/data-card-event-DTO";
 export function BaseUrl (photoURL: string, props: CardEventPageAdministrative){
      const photoUrl =
     props.photos.length > 0
-      ? `http://localhost:4444/uploads/${photoURL}`
+      ? `https://karnaubaapi.onrender.com/uploads/${photoURL}`
       : backgroundloginpage
 
       return photoUrl
